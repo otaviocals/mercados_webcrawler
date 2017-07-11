390914b7930e1f86956059b422e3ee56e99bc9b4
otaviocals <otaviolcals@gmail.com>
2017-07-11 08:50:46 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,263 @@
+#                                                                                                       #
+#   Just Another Webscraper (J.A.W.)                                                                    #
+#                                    v0.1                                                               #
+#                                                                                                       #
+#       written by Otavio Cals                                                                          #
+#                                                                                                       #
+#   Description: A webscrapper for downloading tables and exporting them to .csv files autonomously.    #
+#                                                                                                       #
+#########################################################################################################
+
+#Required External Modules: cython, pygame, kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.textinput import TextInput
+from kivy.uix.spinner import Spinner
+from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.filechooser import FileChooserListView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.effects.scroll import ScrollEffect
+from kivy.core.window import Window
+from kivy.config import Config
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
+from kivy.clock import Clock
+from os.path import join, isdir, expanduser, isfile
+from os import makedirs
+from sys import platform
+import subprocess
+from pathlib import Path
+from kivy.lang import Builder
+from pylibs.file import XFolder
+from pylibs.xpopup import XPopup
+import pylibs.tools
+#import pylibs.win32timezone
+print("hehe")
+from time import sleep
+from unidecode import unidecode
+from pyscripts.google_api_scraper import GoogleApiScraper
+import time
+import datetime
+import gc
+import sys
+import os
+import shutil
+
+######################
+#    Pre-Settings    #
+######################
+
+#Setting configurations
+
+Config.set("kivy","log_enable","0")
+Config.set("kivy","log_level","critical")
+Config.set("graphics","position","custom")
+Config.set("graphics","top","10")
+Config.set("graphics","left","10")
+Config.set("graphics","width","450")
+Config.set("graphics","height","750")
+Config.set("graphics","fullscreen","0")
+Config.set("graphics","borderless","0")
+Config.set("graphics","resizable","0")
+Config.write()
+
+current_os = platform
+
+try:
+    import win32api
+    win32api.SetDllDirectory(sys._MEIPASS)
+except:
+    pass
+
+if current_os.startswith("linux"):
+    slash = "/"
+    phantom = "phantomjs/phantomjs"
+elif current_os.startswith("win32") or current_os.startswith("cygwin"):
+    slash = "\\"
+    phantom = "phantomjs\\phantomjs.exe"
+elif current_os.startswith("darwin"):
+    slash = "/"
+    phantom = "phantomjs/phantomjs"
+else:
+    slash = "/"
+    phantom = "phantomjs/phantomjs"
+
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path,relative_path)
+
+logo_path = resource_path("logo"+slash+"logo.png")
+phantom_path = resource_path(phantom)
+r_script_path = resource_path("rscripts")
+r_libs_path = resource_path("rlibs")
+kivi_app_path = resource_path("kivylibs"+slash+"app_screen.kv")
+config_path = os.path.abspath(".")+slash+"config.txt"
+
+#print(os.path.abspath("."))
+
+#Building GUI
+
+Builder.load_file(kivi_app_path)
+
+
+######################
+#    App Functions   #
+######################
+
+class AppScreen(GridLayout):
+
+    default_folder = expanduser("~") + slash + "Documents"
+    global sel_folder
+    sel_folder = default_folder
+
+#Calling webscraper
+
+    def scrap(self,event):
+        if(not isdir(sel_folder+slash+"logs")):
+            makedirs(sel_folder+slash+"logs")
+        if(not isdir(sel_folder+slash+"proc_data")):
+            makedirs(sel_folder+slash+"proc_data")
+        log_file = open((sel_folder+slash+"logs"+slash+"history.log"),"a",encoding="utf-8")
+
+    #Starting scrap
+
+        print("Starting at "+str(datetime.datetime.now()))
+        log_file.write("Starting at "+str(datetime.datetime.now())+"\n")
+        self.ids.log_output.text += "Starting at "+str(datetime.datetime.now())+"\n"
+
+        print("Webscraper Starting...")
+        log_file.write("Webscraper Starting...\n")
+        self.ids.log_output.text += "Websccraper Starting...\n"
+
+        start_time_seconds = time.time()
+        self.ids.start_button.disabled = True
+
+    #Running webscraper
+
+        str_output_logger = []
+        new_data = Webscraper(sel_folder,log_file,str_output_logger,phantom_path)
+        self.ids.log_output.text += "".join(str_output_logger)
+        gc.collect()
+
+    #Running R scripts
+
+        r_verify = shutil.which("Rscript")
+
+        if(not r_verify == None):
+            print("Running Analysis...")
+            log_file.write("Running Analysis...\n")
+            self.ids.log_output.text += "Running Analysis...\n"
+
+            log_file.close()
+
+            r_output = subprocess.check_output(["Rscript",r_script_path+slash+"analysis.R", slash, sel_folder,config_path, r_script_path,r_libs_path, sel_folder+slash+"logs"+slash+"history.log"],universal_newlines=True)
+            print(r_output)
+
+            log_file = open((sel_folder+slash+"logs"+slash+"history.log"),"a",encoding="utf-8")
+
+        else:
+            print("R compiler not found.")
+            log_file.write("R compiler not found.\n")
+            self.ids.log_output.text += "R compiler not found.\n"
+
+    #Ending scrap
+
+        self.ids.start_button.disabled = False
+        end_time_seconds = time.time()
+        elapsed_time = str(int(round(end_time_seconds - start_time_seconds)))
+
+        print("Ending at "+str(datetime.datetime.now()))
+        log_file.write("Ending at "+str(datetime.datetime.now())+"\n")
+        self.ids.log_output.text += "Ending at "+str(datetime.datetime.now())+"\n"
+
+        print("Total Elapsed Time: "+elapsed_time+" seconds.")
+        log_file.write("Total Elapsed Time: "+elapsed_time+" seconds.\n")
+        self.ids.log_output.text += "Total Elapsed Time: "+elapsed_time+" seconds.\n"
+
+        print("Success!")
+        log_file.write("Success!\n")
+        self.ids.log_output.text += "Success!\n"
+
+        log_file.close()
+
+        self.ids.start_button.state = "normal"
+
+
+#GUI functions
+
+    def start(self,*args):
+        global event
+        if args[1] == "down":
+            self.ids.check_button.disabled = True
+            self.ids.folder_button.disabled = True
+            event = Clock.schedule_once(self.scrap)
+        if args[1] == "normal":
+            self.ids.check_button.disabled = False
+            self.ids.folder_button.disabled = False
+            Clock.unschedule(event)
+
+    def update_button(self,event):
+        self.ToggleButton.text="Stop"
+
+#Directory selection
+
+    def _filepopup_callback(self, instance):
+            if instance.is_canceled():
+                return
+            s = 'Path: %s' % instance.path
+            s += ('\nSelection: %s' % instance.selection)
+            global sel_folder
+            self.ids.text_input.text = "Pasta selecionada:    " + instance.path
+            sel_folder = instance.path
+
+    def _folder_dialog(self):
+        XFolder(on_dismiss=self._filepopup_callback, path=expanduser(u'~'))
+
+
+#Setting window layout
+
+    def __init__(self,**kwargs):
+        super(AppScreen, self).__init__(**kwargs)
+
+        Window.size = (450,750)
+        Window.set_title("Webscraper SUSEP")
+
+
+        self.cols = 1
+        self.size_hint = (None,None)
+        self.width = 450
+        self.height = 750
+        self.icon = logo_path
+        self.ids.ce_logo.source = logo_path
+
+
+
+######################
+#    Starting App    #
+######################
+
+
+class SUSEP(App):
+
+    def build(self):
+        self.icon = logo_path
+        self.resizable = 0
+        self.title = "Webscraper SUSEP"
+        self.log_enable = 0
+        return AppScreen()
+
+######################
+#        Main        #
+######################
+
+if __name__ == "__main__" :
+	SUSEP().run()
---
+++ pyscripts/google_api_scraper.py
@@ -0,0 +1,254 @@
+#                                                                                                       #
+#   Just Another Webscraper (J.A.W.)                                                                    #
+#                                    v0.1                                                               #
+#                                                                                                       #
+#       written by Otavio Cals                                                                          #
+#                                                                                                       #
+#   Description: A webscrapper for downloading tables and exporting them to .csv files autonomously.    #
+#                                                                                                       #
+#########################################################################################################
+
+
+#Required external modules: Selenium, BeautifoulSoup4,
+
+from csv import writer, reader
+from contextlib import closing
+from selenium.webdriver import PhantomJS
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from pathlib import Path
+from os.path import isdir
+from os import makedirs, getcwd, remove
+from sys import platform
+from datetime import datetime, timedelta
+from dateutil.parser import parse
+from urllib.request import urlretrieve
+from glob import glob
+from zipfile import ZipFile
+import sys
+
+######################
+# Webscrapping Stage #
+######################
+
+def GoogleApiScraper(folder, print_output = None, visual_output = None, phantom_path = ""):
+
+    url="http://www2.susep.gov.br/menuestatistica/ses/principal.aspx"
+    rows = []
+    row = []
+    append_to_csv = False
+    current_os = platform
+
+    if current_os.startswith("linux"):
+        slash = "/"
+    elif current_os.startswith("win32") or current_os.startswith("cygwin"):
+        slash = "\\"
+    elif current_os.startswith("darwin"):
+        slash = "/"
+    else:
+        slash = "/"
+
+    folder_data = folder+slash+"data"
+    folder_logs = folder+slash+"logs"
+
+
+#Checking Folders Existence
+
+    if(not isdir(folder)):
+        makedirs(folder)
+    if(not isdir(folder_data)):
+        makedirs(folder_data)
+    if(not isdir(folder_logs)):
+        makedirs(folder_logs)
+
+#Creating date.log if there is no previous file
+
+    if(not Path(folder_logs+slash+"date.log").is_file()):
+        recorded_date = datetime(1970,1,1,0,0)
+        with open(folder_logs+slash+"date.log","w") as f:
+            print(recorded_date,file=f)
+        print("No date.log detected. Creating file...")
+        if not print_output == None:
+            print_output.write("No date.log detected. Creating file...\n")
+        if not visual_output == None:
+            visual_output.append("No date.log detected. Creating file...\n")
+
+#Loading date.log if it exists
+
+    else:
+        with open(folder_logs+slash+"date.log","r") as f:
+            date_log_content = f.readlines()
+            date_log_content = [x.strip() for x in date_log_content]
+            recorded_date = parse(date_log_content[0])
+
+#Getting Raw Data
+
+    with closing(PhantomJS(phantom_path)) as browser:
+
+        browser.implicitly_wait(50)
+        tries = 0
+
+    #Getting Newest Data Date
+
+        browser.get(url)
+        while tries <= 20:
+
+            date_source = browser.find_elements_by_xpath("//form[@id=\"aspnetForm\"]/div[4]/table/tbody/tr/td/table[2]/tbody/tr[9]/td/font/div/span")
+            date_string = date_source[0].text
+
+            if len(date_string) > 0:
+                break
+
+            tries += 1
+        last_date = parse(date_string)
+
+    #Checking if Data is up to date
+
+        if (recorded_date >= last_date):
+            print("Data is up to date.")
+            if not print_output == None:
+                print_output.write("Data is up to date.\n")
+            if not visual_output == None:
+                visual_output.append("Data is up to date.\n")
+            return False
+        else:
+            with open(folder_logs+slash+"date.log","w") as f:
+                print(last_date,file=f)
+
+        #Deleting Old Data
+
+            print("New data detected! Removing old data...")
+            if not print_output == None:
+                print_output.write("New data detected! Removing old data...\n")
+            if not visual_output == None:
+                visual_output.append("New data detected! Removing old data...\n")
+
+            for fl in glob(folder_data+slash+"*"):
+                remove(fl)
+
+        #Downloading Data
+
+            print("Downloading new data...")
+            if not print_output == None:
+                print_output.write("Downloading new data...\n")
+            if not visual_output == None:
+                visual_output.append("Downloading new data...\n")
+
+            data_source = browser.find_elements_by_xpath("//form[@id=\"aspnetForm\"]/div[4]/table/tbody/tr/td/table[2]/tbody/tr[9]/td/font/div/a")
+            data_address = data_source[0].get_attribute("href")
+            data_path = folder_data+slash+"zipped_data.zip"
+            print(data_address)
+            prct_achiev = [0,0,0,0,0,0,0,0,0,0]
+            def dlProgress(count, blockSize, totalSize):
+
+                if(prct_achiev[0] == 0 and (count*blockSize/totalSize) > 0.009 ):
+                    prct_achiev[0] = 1
+                    elap_time = datetime.now() - down_start
+                    down_speed = int((count*blockSize)/((elap_time.seconds*1000) +1))
+                    eta_num = (totalSize)/(down_speed*60000)
+                    eta = timedelta(minutes=int(eta_num), seconds = (eta_num - int(eta_num))*(60))
+                    print("00% Downloaded. Download Speed: "+str(down_speed)+" kB/s Elapsed time: "+str(elap_time).split(".")[0]+" ETA: "+str(eta).split(".")[0])
+
+                if(prct_achiev[1] == 0 and (count*blockSize/totalSize) > 0.1 ):
+                    prct_achiev[1] = 1
+                    elap_time = datetime.now() - down_start
+                    down_speed = int((count*blockSize)/(elap_time.seconds*1000))
+                    eta_num = (totalSize)/(down_speed*60000)
+                    eta = timedelta(minutes=int(eta_num), seconds = (eta_num - int(eta_num))*(60))
+                    print("10% Downloaded. Download Speed: "+str(down_speed)+" kB/s Elapsed time: "+str(elap_time).split(".")[0]+" ETA: "+str(eta).split(".")[0])
+
+                if(prct_achiev[2] == 0 and (count*blockSize/totalSize) > 0.2 ):
+                    prct_achiev[2] = 1
+                    elap_time = datetime.now() - down_start
+                    down_speed = int((count*blockSize)/(elap_time.seconds*1000))
+                    eta_num = (totalSize)/(down_speed*60000)
+                    eta = timedelta(minutes=int(eta_num), seconds = (eta_num - int(eta_num))*(60))
+                    print("20% Downloaded. Download Speed: "+str(down_speed)+" kB/s Elapsed time: "+str(elap_time).split(".")[0]+" ETA: "+str(eta).split(".")[0])
+
+                if(prct_achiev[3] == 0 and (count*blockSize/totalSize) > 0.3 ):
+                    prct_achiev[3] = 1
+                    elap_time = datetime.now() - down_start
+                    down_speed = int((count*blockSize)/(elap_time.seconds*1000))
+                    eta_num = (totalSize)/(down_speed*60000)
+                    eta = timedelta(minutes=int(eta_num), seconds = (eta_num - int(eta_num))*(60))
+                    print("30% Downloaded. Download Speed: "+str(down_speed)+" kB/s Elapsed time: "+str(elap_time).split(".")[0]+" ETA: "+str(eta).split(".")[0])
+
+                if(prct_achiev[4] == 0 and (count*blockSize/totalSize) > 0.4 ):
+                    prct_achiev[4] = 1
+                    elap_time = datetime.now() - down_start
+                    down_speed = int((count*blockSize)/(elap_time.seconds*1000))
+                    eta_num = (totalSize)/(down_speed*60000)
+                    eta = timedelta(minutes=int(eta_num), seconds = (eta_num - int(eta_num))*(60))
+                    print("40% Downloaded. Download Speed: "+str(down_speed)+" kB/s Elapsed time: "+str(elap_time).split(".")[0]+" ETA: "+str(eta).split(".")[0])
+
+                if(prct_achiev[5] == 0 and (count*blockSize/totalSize) > 0.5 ):
+                    prct_achiev[5] = 1
+                    elap_time = datetime.now() - down_start
+                    down_speed = int((count*blockSize)/(elap_time.seconds*1000))
+                    eta_num = (totalSize)/(down_speed*60000)
+                    eta = timedelta(minutes=int(eta_num), seconds = (eta_num - int(eta_num))*(60))
+                    print("50% Downloaded. Download Speed: "+str(down_speed)+" kB/s Elapsed time: "+str(elap_time).split(".")[0]+" ETA: "+str(eta).split(".")[0])
+
+                if(prct_achiev[6] == 0 and (count*blockSize/totalSize) > 0.6 ):
+                    prct_achiev[6] = 1
+                    elap_time = datetime.now() - down_start
+                    down_speed = int((count*blockSize)/(elap_time.seconds*1000))
+                    eta_num = (totalSize)/(down_speed*60000)
+                    eta = timedelta(minutes=int(eta_num), seconds = (eta_num - int(eta_num))*(60))
+                    print("60% Downloaded. Download Speed: "+str(down_speed)+" kB/s Elapsed time: "+str(elap_time).split(".")[0]+" ETA: "+str(eta).split(".")[0])
+
+                if(prct_achiev[7] == 0 and (count*blockSize/totalSize) > 0.7 ):
+                    prct_achiev[7] = 1
+                    elap_time = datetime.now() - down_start
+                    down_speed = int((count*blockSize)/(elap_time.seconds*1000))
+                    eta_num = (totalSize)/(down_speed*60000)
+                    eta = timedelta(minutes=int(eta_num), seconds = (eta_num - int(eta_num))*(60))
+                    print("70% Downloaded. Download Speed: "+str(down_speed)+" kB/s Elapsed time: "+str(elap_time).split(".")[0]+" ETA: "+str(eta).split(".")[0])
+
+                if(prct_achiev[8] == 0 and (count*blockSize/totalSize) > 0.8 ):
+                    prct_achiev[8] = 1
+                    elap_time = datetime.now() - down_start
+                    down_speed = int((count*blockSize)/(elap_time.seconds*1000))
+                    eta_num = (totalSize)/(down_speed*60000)
+                    eta = timedelta(minutes=int(eta_num), seconds = (eta_num - int(eta_num))*(60))
+                    print("80% Downloaded. Download Speed: "+str(down_speed)+" kB/s Elapsed time: "+str(elap_time).split(".")[0]+" ETA: "+str(eta).split(".")[0])
+
+                if(prct_achiev[9] == 0 and (count*blockSize/totalSize) > 0.9 ):
+                    prct_achiev[9] = 1
+                    elap_time = datetime.now() - down_start
+                    down_speed = int((count*blockSize)/(elap_time.seconds*1000))
+                    eta_num = (totalSize)/(down_speed*60000)
+                    eta = timedelta(minutes=int(eta_num), seconds = (eta_num - int(eta_num))*(60))
+                    print("90% Downloaded. Download Speed: "+str(down_speed)+" kB/s Elapsed time: "+str(elap_time).split(".")[0]+" ETA: "+str(eta).split(".")[0])
+
+            down_start = datetime.now()
+            urlretrieve(data_address, data_path, reporthook = dlProgress)
+            print("Download finished!")
+            if not print_output == None:
+                print_output.write("Download finished!\n")
+            if not visual_output == None:
+                visual_output.append("Download finished!\n")
+
+        #Unzipping Data
+
+            with ZipFile(data_path,"r") as zip_ref:
+                zip_ref.extractall(folder_data)
+            remove(data_path)
+            return True
+
+
+######################
+#        Main        #
+######################
+
+if __name__ == "__main__":
+    from sys import argv
+
+    url = input("Enter url to scrap from:\n")
+    folder = input("Enter folder to download to:\n")
+    driver_path = input("Enter driver path:\n")
+
+    print("Starting...")
+
+    Webscraper(url,folder,phantom_path = driver_path)
